f31c9c925ea2ff162934a27638615cf61f85bca4
Manisha RameshBabu <manisharamesh1314@gmail.com>
2026-07-13 17:06:17 +0530

    Add CORS support for frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.database.database import Base, engine
 from app.models.user import User
 from app.models.health_profile import HealthProfile
@@ -6,9 +8,23 @@ from app.models.medical_record import MedicalRecord
 from app.api.auth import router as auth_router
 from app.api.profile import router as profile_router
 from app.api.medical_record import router as medical_record_router
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="MedMind API")
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://med-mind-black.vercel.app",
+        "https://med-mind-p4cmezupb-manisharamesh1314-jpgs-projects.vercel.app",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(profile_router)
